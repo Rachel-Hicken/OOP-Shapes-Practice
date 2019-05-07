59883e3943ec9f45bbb7b3aa45076bc04ddc328a
John Roth <jroth@xitsoftware.com>
2019-05-07 00:36:46 -0600

    clean up

--- Shapes/Shapes/Program.cs
+++ Shapes/Shapes/Program.cs
@@ -16,9 +16,9 @@ namespace Shapes
 //TODO:
 // 0. break into it's own assembly
 // 1. make main look like above
-// 2. display results in a web appn  (extensions and referencing using different lib to get different behavior
+// 2. display results in a web app  (extensions and referencing using different lib to get different behavior
 //        2a. do we need a class library
 // 3. teach entity framework
 // 4. answer question that Rachel may have
-// 5. teach template pattern
+// 5. template pattern
 // Lastly - on your own: review SOLID principles
