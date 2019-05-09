fb88ccadc889e19aa2d6f5bfe69f83faf42177cf
John Roth <jroth@xitsoftware.com>
2019-05-09 11:08:42 -0600

    Added a confirmation test that shapes list contains random shapes.

---
+++ Shapes/ShapeLibraryTests/RandomFactoryExceptionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeLibrary.Factory;
+
+namespace ShapeLibraryTests
+{
+    [TestClass]
+    public class RandomFactoryExceptionTests
+    {
+              
+        public void TestRandomFactoryRadomReturnsNullWhenCreateZero()
+        {
+            var shapes = new RandomShapeFactory().Create(0).Random();
+        }
+
+        public void TestRandomFactoryRadomReturnsNullWhenOnlyCallRandom()
+        {
+            var shapes = new RandomShapeFactory().Random();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ShapeFactoryException), "")]
+        public void RandomFactoryRadomReturnsNullWhenCreateZeroTest()
+        {
+            ExceptionTest.Exception<ShapeFactoryException>("You much specify how many shapes you want to create.",
+                TestRandomFactoryRadomReturnsNullWhenCreateZero);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ShapeFactoryException), "")]
+        public void RandomFactoryRadomReturnsNullWhenOnlyCallRandomTest()
+        {
+            ExceptionTest.Exception<ShapeFactoryException>("You much specify how many shapes you want to create.",
+                TestRandomFactoryRadomReturnsNullWhenOnlyCallRandom);
+        }
+    }
+}
--- Shapes/ShapeLibraryTests/UnitTest1.cs
+++ Shapes/ShapeLibraryTests/UnitTest1.cs
@@ -1,44 +1,20 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeLibrary.Factory;
+using ShapeLibrary.Model.Abstract;
 
 namespace ShapeLibraryTests
 {
     [TestClass]
     public class UnitTest1
     {
-      
-        public void TestRandomFactoryRadomReturnsNullWhenCreateZero()
-        {
-            var shapes = new RandomShapeFactory().Create(0).Random();
-        }
-
-        public void TestRandomFactoryRadomReturnsNullWhenOnlyCallRandom()
-        {
-            var shapes = new RandomShapeFactory().Random();
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ShapeFactoryException), "")]
-        public void RandomFactoryRadomReturnsNullWhenCreateZeroTest()
-        {
-            ExceptionTest.Exception<ShapeFactoryException>("You much specify how many shapes you want to create.",
-                TestRandomFactoryRadomReturnsNullWhenCreateZero);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ShapeFactoryException), "")]
-        public void RandomFactoryRadomReturnsNullWhenOnlyCallRandomTest()
-        {
-            ExceptionTest.Exception<ShapeFactoryException>("You much specify how many shapes you want to create.",
-                TestRandomFactoryRadomReturnsNullWhenOnlyCallRandom);
-        }
-
         [TestMethod]
         public void RandomFactoryRadomReturnsAnObjectTest()
         {
             var shapes = new RandomShapeFactory().Create(1).Random();
             Assert.IsNotNull(shapes);
         }
+
         [TestMethod]
         public void RandomFactoryCreateTest()
         {
@@ -46,6 +22,16 @@ namespace ShapeLibraryTests
             var shapes = new RandomShapeFactory().Create(10).Random();
             Assert.IsNotNull(shapes);
             Assert.AreEqual(expectedValue,shapes.Count);
+        }
+
+        [TestMethod]
+        public void RandomFactoryConfirmRandomTest()
+        {
+            int expectedValue = 10;
+            var shapes = new RandomShapeFactory().Create(10).Random().Select(o=>o as Shape);
+            var firstType = shapes.First().ToString();
+            var different = shapes.Where(o => o.ToString() != firstType).ToList();
+            Assert.IsTrue(different.Any());
         }
     }
 }
