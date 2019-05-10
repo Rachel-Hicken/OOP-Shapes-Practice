6374e051b04ee0596f1b0ab6b5a41d39ab34c048
John Roth <jroth@xitsoftware.com>
2019-05-09 19:23:37 -0600

    add final exam description

--- Shapes/Shapes/Program.cs
+++ Shapes/Shapes/Program.cs
@@ -35,3 +35,9 @@ namespace Shapes
 //        1. check that exception is thrown when _numberRandomShapes is not set 
 //        2. check that the exception that is thrown is my custom exception type (which I will call ShapeFactoryException)
 //        3. validate that the message in the exception thrown in number 2 is this: "You must specify the number of random shapes you want to generate"
+
+
+// FinalExam:  Add triangles, quadrilateral (rectangles, square, trapezoid)... all of which are polygons
+//             Write code that can generate random polygons and display their areas and types to the screen with the sum of the areas on the bottom of the screen (windows console)
+//             Write code above that allows you to do the same on a web page
+//             Note: the user should be able to choose the random option (which prompts the user to enter the number of shapes to display)  OR to use the shapes stored in the database (make sure there is sample data in the database for all polygons)
